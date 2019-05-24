c458b2a688a6e12e5a6d72483c89727829c2af66
typhoonzero <typhoonzero1986@gmail.com>
2019-05-24 10:55:59 +0800

    test coverage

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,4 +10,10 @@ else
 fi
 
 docker build -t sqlflow/sqlflow:$DOCKER_TAG -f ./Dockerfile .
-docker push sqlflow/sqlflow:$DOCKER_TAG
+# Test coverage note pushing images
+# docker push sqlflow/sqlflow:$DOCKER_TAG
+
+echo $COVERALLS_TOKEN
+
+docker run --rm -it -v $GOPATH:/go sqlflow/sqlflow:$DOCKER_TAG \
+bash -c "go get golang.org/x/tools/cmd/cover && go get github.com/mattn/goveralls && /go/bin/goveralls --help"
